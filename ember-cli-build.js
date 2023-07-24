d337afb52d1dcc46afc3913011bd95348907cb5f
Achraf Atauil <achrafatauil@hotmail.com>
2023-07-24 16:19:11 +0200

    add simple-auth & appuniversum packages

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    '@appuniversum/ember-appuniversum': {
+      dutchDatePickerLocalization: true,
+      disableWormholeElement: true,
+    },
+    'ember-simple-auth': {
+      useSessionSetupMethod: true,
+    },
   });
 
   return app.toTree();

4303ea25eaa098f1488dcd088d98ab218c813f10
Achraf Atauil <achrafatauil@hotmail.com>
2023-08-24 14:23:48 +0200

    npm run lint:fix

--- app/models/subsidy-measure-offer.js
+++ app/models/subsidy-measure-offer.js
@@ -2,7 +2,7 @@ import Model, { attr } from '@ember-data/model';
 
 export default class SubsidyMeasureOfferModel extends Model {
   @attr() title;
-  
+
   @attr() subsidyApplicationFlow;
   @attr() subsidyApplicationFlowStep;
   @attr() subsidyMeasureConsumptionStatus;

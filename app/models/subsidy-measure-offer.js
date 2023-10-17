1b0be62be38918c0f2d64c64616ed39a00ead380
Achraf Atauil <achrafatauil@hotmail.com>
2023-10-17 16:49:43 +0200

    sync data model files with state in frontend-loket

--- app/models/subsidy-measure-offer.js
+++ app/models/subsidy-measure-offer.js
@@ -1,13 +1,12 @@
 import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class SubsidyMeasureOfferModel extends Model {
-  // @attr() title;
-  // @attr() subsidyApplicationFlow;
-  // @attr() subsidyApplicationFlowStep;
-  // @attr() subsidyMeasureConsumptionStatus;
-
-  // TODO: top was original code, bottom is from loket
   @attr title;
   @attr externalInformation;
-  @hasMany('subsidy-measure-offer-series') series;
+
+  @hasMany('subsidy-measure-offer-series', {
+    async: true,
+    inverse: 'subsidyMeasureOffer',
+  })
+  series;
 }

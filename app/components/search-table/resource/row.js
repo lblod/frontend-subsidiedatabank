4a70f921be13a7400ae3049ca3a5e3abb32c817c
Achraf <achrafatauil@hotmail.com>
2024-02-14 14:35:08 +0100

    add fietssubsidie to allowedSubsidies list

--- app/components/search-table/resource/row.js
+++ app/components/search-table/resource/row.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 
 const allowedSubsidies = new Set([
   '64d40351-8128-464f-990f-41066154583e', //Lokaal Energie- en Klimaatpact 1.0
+   '70cc4947-33a3-4d26-82e0-2e1eacd2fea2', //Fietssubsidie
 ]);
 
 export default class RowComponent extends Component {

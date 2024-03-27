bb93ff008b5bde3bab0375047a944637d0b9e2f7
Achraf <achrafatauil@hotmail.com>
2024-03-27 14:55:46 +0100

    npm run lint:fix

--- app/components/search-table/resource/row.js
+++ app/components/search-table/resource/row.js
@@ -2,18 +2,20 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 
 const allowedSubsidies = new Set([
-   '64d40351-8128-464f-990f-41066154583e', // Lokaal Energie- en Klimaatpact 1.0
-   '70cc4947-33a3-4d26-82e0-2e1eacd2fea2', // Fietssubsidie
-   'be6311ab-3a16-478c-9cc1-a233439c44b5', // Oekraine Nooddorpen
-   'b03215cf-2206-493c-b534-0546a2479eef', // Oekraine Opknapwerken slaapplekken
+  '64d40351-8128-464f-990f-41066154583e', // Lokaal Energie- en Klimaatpact 1.0
+  '70cc4947-33a3-4d26-82e0-2e1eacd2fea2', // Fietssubsidie
+  'be6311ab-3a16-478c-9cc1-a233439c44b5', // Oekraine Nooddorpen
+  'b03215cf-2206-493c-b534-0546a2479eef', // Oekraine Opknapwerken slaapplekken
 ]);
-
 
 export default class RowComponent extends Component {
   @service features;
 
   get isViewableSubsidy() {
-    if (this.args.consumption.status.get('id') == "6373b454-22b6-4b65-b98f-3d86541f2fcf") {
+    if (
+      this.args.consumption.status.get('id') ==
+      '6373b454-22b6-4b65-b98f-3d86541f2fcf'
+    ) {
       return false;
     }
 

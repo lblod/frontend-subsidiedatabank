478c2a2a850e7020ee3031c3a0a7a7f5d9d4233f
Achraf <achrafatauil@hotmail.com>
2025-10-14 19:12:58 +0200

    Update config files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,19 +2,53 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function (defaults) {
+module.exports = async function (defaults) {
+  const { setConfig } = await import('@warp-drive/build-config');
+
   const app = new EmberApp(defaults, {
-    '@appuniversum/ember-appuniversum': {
-      dutchDatePickerLocalization: true,
-      disableWormholeElement: true,
-    },
-    'ember-test-selectors': {
-      strip: false,
+    // This is needed when `staticEmberSource` is enabled
+    'ember-fetch': {
+      preferNative: true,
+      nativePromise: true,
     },
     'ember-simple-auth': {
       useSessionSetupMethod: true,
     },
+    'ember-test-selectors': {
+      strip: false,
+    },
+    '@lblod/ember-submission-form-fields': {
+      helpTextBelowLabel: true,
+    },
+    '@appuniversum/ember-appuniversum': {
+      dutchDatePickerLocalization: true,
+      disableWormholeElement: true,
+    },
   });
 
-  return app.toTree();
+
+  setConfig(app, __dirname, {
+    deprecations: {
+      DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
+    },
+  });
+  // Uncomment this if you want a "classic build"
+  // return app.toTree();
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    staticEmberSource: true,
+    splitAtRoutes: [
+      'mock-login',
+    ],
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
 };

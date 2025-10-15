2c51f686d7f5c24afda3c92f01dc7c00718da429
Achraf <achrafatauil@hotmail.com>
2025-10-15 16:21:32 +0200

    remove sentry and deprecate _super

--- app/app.js
+++ app/app.js
@@ -4,10 +4,8 @@ import loadInitializers from 'ember-load-initializers';
 import browserUpdate from 'browser-update';
 import config from './config/environment';
 import './config/custom-inflector-rules';
-import { setupSentry } from 'frontend-subsidiedatabank/utils/sentry';
 import { silenceEmptySyncRelationshipWarnings } from './utils/ember-data';
 
-setupSentry();
 silenceEmptySyncRelationshipWarnings();
 
 export default class App extends Application {
@@ -15,14 +13,6 @@ export default class App extends Application {
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
 }
-
-// By default `Array.prototype._super` is enumerable which causes conflicts with rdflib.
-// We can force it to be non-enumerable so everything works as expected.
-// This code can be removed once the bug is fixed and released, or if we disable the prototype extensions in the app.
-// More information: https://github.com/emberjs/ember.js/issues/19289
-Object.defineProperty(Array.prototype, '_super', {
-  enumerable: false,
-});
 
 browserUpdate({
   vs: { i: 11, f: -3, o: -3, s: -3, c: -3 },

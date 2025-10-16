e5f71b313db54b98c9b5f3d777f812609c1b89af
Achraf <achrafatauil@hotmail.com>
2025-10-16 11:12:33 +0200

    remvove unneeded ""new" route
    subsidiedatbaank is not capable of creating new subsidies

--- app/router.js
+++ app/router.js
@@ -13,7 +13,6 @@ Router.map(function () {
     this.route('applications');
     this.route('detail', { path: '/:id' }, function () {
       this.route('step', { path: '/steps/:step_id' }, function () {
-        this.route('new');
         this.route('detail', { path: '/forms/:form_id' });
         this.route('skipped');
       });

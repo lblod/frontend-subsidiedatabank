060db8dbb1ae53914fc3dc53621cab5022d0df5b
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-11-20 12:06:19 +0100

    Resolve more prototype extension issues
    Some code needed to be adjusted after disabling the prototype extensions.

--- app/components/search-table/resource/row.js
+++ app/components/search-table/resource/row.js
@@ -4,6 +4,11 @@ import { inject as service } from '@ember/service';
 export default class RowComponent extends Component {
   @service features;
 
+  get firstParticipation() {
+    // We assume the data was included and loaded before the component renders, so we don't have to deal with the async states.
+    return this.args.consumption.hasMany('participations').value().at(0);
+  }
+
   get isViewableSubsidy() {
     if (
       this.args.consumption.status.get('id') ==
--- app/controllers/subsidy/detail.js
+++ app/controllers/subsidy/detail.js
@@ -30,10 +30,6 @@ export default class SubsidyDetailController extends Controller {
     return this.model.consumption.participations;
   }
 
-  get organization() {
-    return this.participations?.firstObject?.participatingOrganization;
-  }
-
   get canDelete() {
     return this.model.consumption.get('status.isConcept');
   }
